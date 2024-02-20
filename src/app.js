dc8e6e2126b25113dfdf737a46c6b1c42858b04d
MathieuCoding <polletmathieu.pro@gmail.com>
2024-02-20 14:40:26 +0100

    addItem and removeItem updated and viewStock added

--- src/app.js
+++ src/app.js
@@ -22,12 +22,26 @@ class Basket {
         this.items = [];
     }
 
-    addItem(name, price) {
-        return this.items.push({name, price});
+    addItem(name, quantity, price) {
+        const existingItemIndex = this.items.findIndex(item => item.name === name);
+
+        if (existingItemIndex !== -1) {
+            this.items[existingItemIndex].quantity += quantity;
+        } else {
+            this.items.push({ name, quantity, price });
+        }
     }
 
-    removeItem(name) {
-        this.items = this.items.filter(item => item.name !== name);
+    removeItem(name, quantity) {
+        const existingItemIndex = this.items.findIndex(item => item.name === name);
+
+        if (existingItemIndex !== -1) {
+            this.items[existingItemIndex].quantity -= quantity;
+
+            if (this.items[existingItemIndex].quantity <= 0) {
+                this.items.splice(existingItemIndex, 1);
+            }
+        }
     }
 
     getTotalPrice() {
@@ -45,6 +59,14 @@ class Basket {
             return item;
         });
     }
+
+    viewStock() {
+        console.log('The stock:');
+        this.items.forEach(item => {
+            console.log(`${item.name}: ${item.quantity} units, ${item.price}€ each`);
+        });
+    }
+
 
 }
 
